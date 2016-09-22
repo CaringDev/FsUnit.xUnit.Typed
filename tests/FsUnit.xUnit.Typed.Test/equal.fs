3be68f943f7e9887fffd781897d55e1265356b7f
Raphael Schweizer <code@caringdev.ch>
2016-09-22 02:52:16 +0200

    initial (incl. fail, equal)

---
+++ tests/FsUnit.xUnit.Typed.Test/equal.fs
@@ -0,0 +1,24 @@
+﻿module ``Equal``
+
+open Xunit
+open FsUnit.Xunit.Typed
+open Matchers
+
+[<Fact>]
+let ``succeeds when value equal``() =
+    42 |> should equal 42
+
+[<Fact>]
+let ``succeeds when reference equal``() =
+    let o = obj()
+    o |> should equal o
+
+[<Fact>]
+let ``fails when value non-equal``() =
+    Assert.Throws<MatchException>(fun () ->
+        42 |> should equal 7)
+
+[<Fact>]
+let ``fails when reference non-equal``() =
+    Assert.Throws<MatchException>(fun () ->
+        obj() |> should equal (obj()))
